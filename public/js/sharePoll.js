c8681360e6e517f92d42f8cd2fa797002060288a
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2018-01-10 15:54:53 +0530

    Added Share button

---
+++ public/js/sharePoll.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#share").on('click', function(){
+      var TweetURL = 'https://twitter.com/intent/tweet?text='
+      var PollID = window.location.pathname.slice(7);
+      var PageURL = 'https://votem3-hemakshis.herokuapp.com/polls' + PollID;
+      var text = 'Hey! Check out this poll on ' + PageURL + ' #votem3.'
+      window.open(TweetURL + encodeURIComponent(text));
+  });
+});
