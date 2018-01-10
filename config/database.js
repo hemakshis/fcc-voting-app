cff6e554294db8f95a8f2212ea08f0ab20e9d1d9
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2018-01-10 15:57:19 +0530

    Added Share button

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
-/*
 module.exports = {
   database:'mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@ds159696.mlab.com:59696/votem3',
   secret:'nonu'
 }
-*/
-
+/*
 module.exports = {
   database: 'mongodb://localhost:27017/votem3',
   secret: 'nonu'
 }
+*/
